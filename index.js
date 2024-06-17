72cec401413e0da383571a8acbe8b976da659474
LindaTa <lindata@maneandmoon.com>
2024-06-17 13:11:08 -0500

    intro to js array lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+  const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+cats.push(name);
+return cats;
+}
+
+function destructivelyPrependCat(name) {
+cats.unshift(name);
+return cats;    
+}
+
+function destructivelyRemoveLastCat(name) {
+cats.pop(name);
+return cats;    
+}
+
+function destructivelyRemoveFirstCat(name) {
+cats.shift(name);
+return cats;    
+}
+
+function appendCat(name) {
+var newArray = cats.slice(name);
+newArray.push(name);
+return newArray;
+}
+
+function prependCat(name) {
+var newArray = cats.slice(name);
+newArray.unshift(name);
+return newArray;    
+}
+
+function removeLastCat(name) {
+var newArray = cats.slice(name);
+newArray.pop(name);
+return newArray;    
+}
+
+function removeFirstCat(name) {
+var newArray = cats.slice(name);
+newArray.shift(name);
+return newArray;    
+}
